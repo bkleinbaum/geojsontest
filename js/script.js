a535a05de4ce26d976997d15a5f65abd44ac118c
bkleinbaum <benkleinbaum@Bens-MacBook-Pro.local>
2014-11-08 17:33:03 -0500

    need to fix one 2500 geojson layer
    and play with css

--- js/script.js
+++ js/script.js
@@ -8,21 +8,100 @@ L.tileLayer('http://{s}.tiles.mapbox.com/v3/examples.map-i875mjb7/{z}/{x}/{y}.pn
 }).addTo(map);
 
 
-function makeMarkers(feature,layer) {
+function popUp(feature, layer) {
+  layer.bindPopup(feature.properties.name);
+}
+
+var to1000 = new L.GeoJSON.AJAX("/js/0-1000.geojson", {onEachFeature:makeMarkers
+});
+// to1000.addTo(map);
+
+// function make0to1000Markers(feature,layer) {
+// 	layer.bindPopup(
+// 		"This lot is "
+// 		+feature.properties.sqft+" square feet"
+// 		+"<br> It is under the jurisdiction of "
+// 		+feature.properties.agency)
+// };
+
+
+// var toTwentyFive = new L.GeoJSON.AJAX("/js/1000-2500.geojson", {onEachFeature:makeMarkers2500
+// });
+// toTwentyFive.addTo(map);
+
+// function makeMarkers2500 (feature,layer) {
+// 	layer.bindPopup(
+// 		"This lot is "
+// 		+feature.properties.sqft+" square feet"
+// 		+"<br> It is under the jurisdiction of "
+// 		+feature.properties.agency 
+// 		)};
+
+
+var toFive = new L.GeoJSON.AJAX("/js/2500-5000.geojson", {onEachFeature:makeMarkers
+});
+// toFive.addTo(map);
+
+// function makeMarkers5000 (feature,layer) {
+// 	layer.bindPopup(
+// 		"This lot is "
+// 		+feature.properties.sqft+" square feet"
+// 		+"<br> It is under the jurisdiction of "
+// 		+feature.properties.agency 
+// 		)};
+
+
+var toTen = new L.GeoJSON.AJAX("/js/5000-10000.geojson", {onEachFeature:makeMarkers
+});
+// toTen.addTo(map);
+
+// function makeMarkers10000 (feature,layer) {
+// 	layer.bindPopup(
+// 		"This lot is "
+// 		+feature.properties.sqft+" square feet"
+// 		+"<br> It is under the jurisdiction of "
+// 		+feature.properties.agency 
+// 		)};
+
+var toInfinty = new L.GeoJSON.AJAX("/js/10000.geojson", {onEachFeature:makeMarkers
+});
+// toInfinty.addTo(map);
+
+// function makeMarkersInfinty (feature,layer) {
+// 	layer.bindPopup(
+// 		"This lot is "
+// 		+feature.properties.sqft+" square feet"
+// 		+"<br> It is under the jurisdiction of "
+// 		+feature.properties.agency+"." 
+// 		)};
+
+function makeMarkers (feature,layer) {
 	layer.bindPopup(
-		feature.properties.Address
-		+"<br> The Zoning is "
-		+feature.properties.AllZoning1), 
-		$('#bottombar').append(feature.properties.AllZoning1);
+		"This lot is "
+		+feature.properties.sqft+" square feet."
+		+"<br> It is under the jurisdiction of "
+		+feature.properties.agency+"." 
+		)};
+
+
+
+var size1000 = L.layerGroup([to1000]);
+var size5000 = L.layerGroup([toFive]);
+var size10000 = L.layerGroup([toTen]);
+var sizeInfinty = L.layerGroup([toInfinty])
+
+var overlays = {
+	"Up to 1000 square feet" : size1000,
+	"2501 to 5000 square feet": size5000,
+	"5001 to 10000 square feet": size10000,
+	"More than 10000 square feet": sizeInfinty
 };
 
-
-
-$.getJSON('/js/cityown.geojson', function (data) {
-	L.geoJson(data.features, {
-	onEachFeature: makeMarkers
-	}).addTo(map);
-
+layerControl = L.control.layers(null, overlays, {
+	position: 'bottomright',
+	collapsed: false
 });
 
-	
+
+layerControl.addTo(map);
+
